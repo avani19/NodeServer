258ed5608fb859243cb3380a221c56c3c8bc48e5
Avani Ghetia <Avani@Avanis-MacBook-Pro.local>
2016-10-24 20:25:29 -0400

    Good Bad

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var http = require('http');
 // declair port
 var PORTGOOD = 7000;
 var PORTBAD = 7500;
+var randomPhrases = ['You are nice.', 'You smells good.', 'You are looking cute!','You are so kind'];
+var randomBadPhrases = ['You are worst.', 'You smells bad.', 'You are looking ugly.', 'You are liar!'];
 // function to handle
-// function goodAboutU(){
 function handleRequestsGood(request, response){
-  response.end('You are good');
+  response.end(randomPhrases[Math.floor(Math.random()*randomPhrases.length)]);
 }
 
 function handleRequestsBad(request, response){
-  response.end('You are worst');
+  // response.end('You are worst');
+  response.end(randomBadPhrases[Math.floor(Math.random()*randomBadPhrases.length)]);
 }
 // instantiate our server
 var serverGood = http.createServer(handleRequestsGood);
